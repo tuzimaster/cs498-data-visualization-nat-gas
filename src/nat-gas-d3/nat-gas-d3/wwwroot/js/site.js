d2d1c4c02cf85500b2b4e7f0d090e7bc46f9132f
Alexander Matusiak <alexander.matusiak.1@gmail.com>
2018-07-28 11:36:26 -0600

    Updating the views to graph residential and commercial price over time.

--- src/nat-gas-d3/nat-gas-d3/wwwroot/js/site.js
+++ src/nat-gas-d3/nat-gas-d3/wwwroot/js/site.js
@@ -1,55 +1,106 @@
 ﻿$(document).ready(function () {
     //SetUpResidentialAndCommercialPriceGraph();
     SetupProductionOfNiobaraBase();
+    SetUpResidentialAndCommercialPriceGraph();
 });
 
-/*function SetUpResidentialAndCommercialPriceGraph() {
-    var parseDate = d3.timeParse("%m/%d/%Y");
+function SetUpResidentialAndCommercialPriceGraph() {
+    console.log("Setup Residential/Commercial Graph");
+    // set the dimensions and margins of the graph
+    var margin = { top: 20, right: 20, bottom: 30, left: 50 },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
-    var svg = d3.select("#residentialCommercial"),
-        margin = { top: 20, right: 80, bottom: 30, left: 50 },
-        width = svg.attr("width") - margin.left - margin.right,
-        height = svg.attr("height") - margin.top - margin.bottom,
-        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    // parse the date / time
+    var parseTime = d3.timeParse("%m/%d/%Y");
 
-    var parseTime = d3.timeParse("%Y%m%d");
+    // set the ranges
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
 
-    var x = d3.scaleTime().range([0, width]),
-        y = d3.scaleLinear().range([height, 0]),
-        z = d3.scaleOrdinal(d3.schemeCategory10);
+    // append the svg obgect to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg = d3.select("#residentialCommercial")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
 
-    var line = d3.line()
-        .curve(d3.curveBasis)
-        .x(function (d) { return x(d.date); })
-        .y(function (d) { return y(d.temperature); });
-
-
-    d3.csv("data/Natural_Gas_Prices_in_Colorado.csv",
+    // Get the data
+    console.log("get the data from csv");
+    d3.csv("/data/Natural_Gas_Prices_in_Colorado.csv",
         function (error, data) {
+            console.log("Get the data");
+            // format the data
 
-        });
-    .row(function (d) {
-            return {
-                date: parseDate(d.date),
-                residentialPrice: Number(d.residentialPrice),
-                commercialPrice: Number(d.commercialPrice)
+            console.log(data);
+            var mappedData = new Array(data.length);
+
+            for (var i = 0; i < data.length; i++) {
+                mappedData[i] = {
+                    date: parseTime(data[i].date),
+                    residentialPrice: Number(data[i].residentialPrice),
+                    commercialPrice: Number(data[i].commercialPrice)
+                };
             }
-        })
-        .get(function (error, data) {
-            var height = 400;
-            var width = 600;
 
-            var maxDate = d3.max(data, function (d) { return d.date });
-            var minDate = d3.min(data, function (d) { return d.date });
-            var maxResidentialPrice = d3.max(data, function (d) { return d.residentialPrice });
-            var maxCommercialPrice = d3.max(data, function (d) { return d.commercialPrice });
+            console.log(mappedData);
 
-            console.log("Max Date: " + maxDate);
-            console.log("Min Date: " + minDate);
-            console.log("Max Residential Price: " + maxResidentialPrice);
-            console.log("Max Commercial Price: " + maxCommercialPrice);
+            console.log("get the scales");
+            // Scale the range of the data
+            x.domain(d3.extent(mappedData, function (d) { return d.date; }));
+
+            var maxCommercialPrice =  d3.max(mappedData, function (d) { return d.commercialPrice; });
+            var maxResidentialPrice = d3.max(mappedData, function (d) { return d.residentialPrice; });
+
+            var maxPrice = null;
+
+            if (maxResidentialPrice >= maxCommercialPrice) {
+                maxPrice = maxResidentialPrice;
+            } else {
+                maxPrice = maxCommercialPrice;
+            }
+
+            console.log("Max Price: " + maxPrice);
+
+            y.domain([0, maxPrice]);
+
+            console.log("add the line");
+            // Add the valueline path.
+            var residentialPriceLine = d3.line()
+                .x(function (d) { return x(d.date); })
+                .y(function (d) { return y(d.residentialPrice); });
+
+            var commercialPriceLine = d3.line()
+                .x(function (d) { return x(d.date); })
+                .y(function (d) { return y(d.commercialPrice); });
+
+            var lineGraph = svg.append("path")
+                .attr("d", residentialPriceLine(mappedData))
+                .attr("stroke", "blue")
+                .attr("stroke-width", 2)
+                .attr("fill", "none");
+
+            var commercialLineGraph = svg.append("path")
+                .attr("d", commercialPriceLine(mappedData))
+                .attr("stroke", "red")
+                .attr("stroke-width", 2)
+                .attr("fill", "none");
+
+            console.log("add the x axis");
+            // Add the X Axis
+            svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x));
+
+            console.log("add the y axis");
+            // Add the Y Axis
+            svg.append("g")
+                .call(d3.axisLeft(y));
         });
-}*/
+}
 
 function SetupProductionOfNiobaraBase() {
     // set the dimensions and margins of the graph
